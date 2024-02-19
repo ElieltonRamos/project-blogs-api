753ee945927fe95ff44d8ee5f2b8c1fa42b55f94
Elielton <elieltonramos14@gmail.com>
2024-02-19 16:48:47 -0300

    refatorando midleware JWT

--- src/middlewares/checkToken.js
+++ src/middlewares/checkToken.js
@@ -2,9 +2,9 @@ const { decodeToken } = require('../utils/tokenAuth');
 
 const checkToken = (req, res, next) => {
   const { authorization } = req.headers;
-  const token = authorization.split(' ')[1];
-
   if (!authorization) return res.status(401).json({ message: 'Token not found' });
+  
+  const token = authorization.split(' ')[1];
   if (!token) return res.status(401).json({ message: 'Expired or invalid token' });
 
   const user = decodeToken(token);
